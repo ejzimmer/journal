5eec5c3f2d6da4f7357bffcd6521da0972560140
Erin Zimmer <ejzimmer@gmail.com>
2022-01-04 09:54:09 +1100

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/tabs"
+import React from "react"
 
-function App() {
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Tabs>
+      <TabList>
+        <Tab>Habits</Tab>
+        <Tab isDisabled>Todo Today</Tab>
+        <Tab isDisabled>Todo</Tab>
+        <Tab isDisabled>Meetings</Tab>
+        <Tab isDisabled>Work hours</Tab>
+        <Tab isDisabled>Period</Tab>
+      </TabList>
 
-export default App;
+      <TabPanels>
+        <TabPanel>
+          <p>one!</p>
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
+  )
+}
