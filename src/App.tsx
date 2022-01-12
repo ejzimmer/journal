0b3a2d91015edc35daf0506f97d9c4c32d96b6e0
Erin Zimmer <ejzimmer@gmail.com>
2022-01-12 22:05:16 +1100

    login with redirect

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,34 @@ import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/tabs"
 import { Track } from "./tabs/Track"
 import { Todo } from "./tabs/Todo"
 import { Today } from "./tabs/Today"
-import { FirebaseContext } from "./shared/FirebaseContext"
-import { useContext, useState } from "react"
 import { Button } from "@chakra-ui/button"
 import { Grid } from "@chakra-ui/layout"
 
+import {
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithRedirect,
+} from "firebase/auth"
+import { useState } from "react"
+
 export function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
-  const { login, getUser } = useContext(FirebaseContext)
+  const [isLoggedIn, setLoggedIn] = useState(false)
+  const auth = getAuth()
 
-  const logMeIn = async () => {
-    if (!login || !getUser) return
+  onAuthStateChanged(auth, () => {
+    setLoggedIn(!!auth.currentUser)
+  })
 
-    await login()
-    setIsLoggedIn(!!getUser())
+  const login = () => {
+    const provider = new GoogleAuthProvider()
+    signInWithRedirect(auth, provider)
   }
 
   if (!isLoggedIn) {
     return (
       <Grid height="100vh">
-        <Button m="auto" onClick={logMeIn}>
+        <Button m="auto" onClick={login}>
           Log In
         </Button>
       </Grid>
