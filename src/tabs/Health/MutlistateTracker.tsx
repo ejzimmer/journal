043356af822879a6663eeeab4b9301dc7285f421
Erin Zimmer <ejzimmer@gmail.com>
2022-07-28 17:50:33 +1000

    multistate tracker

---
+++ src/tabs/Health/MutlistateTracker.tsx
@@ -0,0 +1,65 @@
+import { Box, VisuallyHidden, FormLabel } from "@chakra-ui/react"
+import { ChangeEventHandler, useState } from "react"
+
+type Props = {
+  name: string
+  options: string[]
+  value: string
+  onChange: (value: string) => void
+}
+
+export function MultistateTracker({ name, options, value, onChange }: Props) {
+  return (
+    <Box
+      as="fieldset"
+      __css={{
+        label: { display: "none" },
+        "input:checked + label": { display: "revert" },
+      }}
+    >
+      {options.map((option, index) => (
+        <MultiStateCheckbox
+          key={option}
+          htmlFor={options[(index + 1) % options.length]}
+          isChecked={value === option}
+          name={name}
+          onChange={(event) => {
+            onChange(event.target.value)
+          }}
+          value={option}
+        />
+      ))}
+    </Box>
+  )
+}
+
+function MultiStateCheckbox({
+  name,
+  value,
+  isChecked,
+  onChange,
+  htmlFor,
+}: {
+  name: string
+  value: string
+  isChecked: boolean
+  onChange: ChangeEventHandler<HTMLInputElement>
+  htmlFor: string
+}) {
+  return (
+    <>
+      <VisuallyHidden
+        as="input"
+        type="radio"
+        id={`${name}-${value}`}
+        name={name}
+        value={value}
+        checked={isChecked}
+        onChange={onChange}
+      />
+      <FormLabel cursor="pointer" htmlFor={`${name}-${htmlFor}`}>
+        {value}
+      </FormLabel>
+    </>
+  )
+}
--- src/tabs/Health/index.tsx
+++ src/tabs/Health/index.tsx
@@ -1,37 +1,78 @@
 import { useState } from "react"
 import { BooleanTracker } from "./BooleanTracker"
+import { MultistateTracker } from "./MutlistateTracker"
 
-const TRACKERS = {
+type TrackerBoolean = {
+  id: string
+  label: string
+  isChecked: boolean
+}
+
+type TrackerMultistate = {
+  id: string
+  options: string[]
+  value: string
+}
+
+type Tracker = TrackerBoolean | TrackerMultistate
+type Trackers = Record<string, Tracker>
+
+const TRACKERS: Trackers = {
   stretch: { id: "stretch", label: "🧘🏽", isChecked: false },
   calories: { id: "calories", label: "⚖️", isChecked: false },
   teeth: { id: "teeth", label: "🦷", isChecked: false },
+  drinks: { id: "drinks", options: ["🫖", "🍺", "🍻"], value: "🫖" },
+  period: { id: "period", options: ["⚪", "🐞", "🔴"], value: "⚪" },
 }
 
 export function Health() {
   const [state, setState] = useState(TRACKERS)
 
+  const updateTracker = (tracker: Tracker, key: string, value: any) => {
+    setState((state) => ({
+      ...state,
+      [tracker.id]: {
+        ...tracker,
+        [key]: value,
+      },
+    }))
+  }
+
   return (
     <>
-      {Object.values(state).map((value) => (
-        <BooleanTracker
-          key={value.id}
-          isChecked={value.isChecked}
-          onChange={(isChecked) =>
-            setState((state) => ({
-              ...state,
-              [value.id]: { ...value, isChecked },
-            }))
-          }
-        >
-          {value.label}
-        </BooleanTracker>
-      ))}
+      {Object.values(state).map((tracker) => {
+        if (isMultistate(tracker)) {
+          return (
+            <MultistateTracker
+              key={tracker.id}
+              name={tracker.id}
+              options={tracker.options}
+              value={tracker.value}
+              onChange={(value) => updateTracker(tracker, "value", value)}
+            />
+          )
+        }
+
+        return (
+          <BooleanTracker
+            key={tracker.id}
+            isChecked={tracker.isChecked}
+            onChange={(isChecked) =>
+              updateTracker(tracker, "isChecked", isChecked)
+            }
+          >
+            {tracker.label}
+          </BooleanTracker>
+        )
+      })}
     </>
   )
 }
 
+function isMultistate(tracker: Tracker): tracker is TrackerMultistate {
+  return (tracker as TrackerMultistate).options !== undefined
+}
+
 /**
- * alcohol
  * waist
- * period
  */
