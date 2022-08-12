c9158d5544222772cf5d1423d9df52ef56d454e9
Erin Zimmer <ejzimmer@gmail.com>
2022-08-12 21:41:20 +1000

    basic month navigation

--- src/tabs/Health/index.tsx
+++ src/tabs/Health/index.tsx
@@ -2,9 +2,15 @@
 // navigate between months & make start day right
 // show streaks
 
-import { Grid, GridItem } from "@chakra-ui/react"
-import { format, getDaysInMonth } from "date-fns"
-import { useCallback, useContext, useEffect, useMemo, useState } from "react"
+import { Button, Grid, GridItem, Heading } from "@chakra-ui/react"
+import {
+  addMonths,
+  format,
+  getDaysInMonth,
+  startOfMonth,
+  subMonths,
+} from "date-fns"
+import { useCallback, useContext, useEffect, useState } from "react"
 import { FirebaseContext } from "../../shared/FirebaseContext"
 import { getWeekdays } from "../../shared/utilities"
 import { Day } from "./Day"
@@ -21,47 +27,56 @@ const getEmptyMonth = (date: Date): Record<string, Trackers> => {
 export function Health() {
   const { read, write } = useContext(FirebaseContext)
 
-  const today = useMemo(() => new Date(), [])
-  const [days, setDays] = useState(getEmptyMonth(today))
+  const [date, setDate] = useState(startOfMonth(new Date()))
+  const [days, setDays] = useState(getEmptyMonth(date))
 
-  const weekdays = getWeekdays(today)
+  const weekdays = getWeekdays(date)
 
   useEffect(() => {
-    read(`health/${format(today, "yyyy/MM")}`, (value) => {
-      setDays((days) => ({ ...days, ...value }))
+    read(`health/${format(date, "yyyy/MM")}`, (value) => {
+      setDays({ ...getEmptyMonth(date), ...value })
     })
-  }, [read, today])
+  }, [read, date])
 
   const onChange = useCallback(
     (trackers: Trackers, day: string) => {
-      write(`health/${format(today, "yyyy/MM")}/${day}`, trackers)
+      write(`health/${format(date, "yyyy/MM")}/${day}`, trackers)
     },
-    [write, today]
+    [write, date]
   )
 
   return (
-    <Grid
-      gridTemplateColumns="repeat(7, min-content)"
-      justifyContent="center"
-      justifyItems="center"
-    >
-      {weekdays.map((day) => (
-        <GridItem textTransform="uppercase" color="gray.400" mb="2">
-          {format(day, "EEE")}
-        </GridItem>
-      ))}
-      {Object.entries(days)
-        .sort(([a], [b]) => Number.parseInt(a) - Number.parseInt(b))
-        .map(([day, trackers]) => (
-          <Day
-            key={day}
-            day={day}
-            trackers={trackers}
-            mx="2px"
-            my="8px"
-            onChange={onChange}
-          />
+    <>
+      <Heading>{format(date, "MMMM")}</Heading>
+      <Button onClick={() => setDate((date) => subMonths(date, 1))}>
+        Back
+      </Button>
+      <Button onClick={() => setDate((date) => addMonths(date, 1))}>
+        Forward
+      </Button>
+      <Grid
+        gridTemplateColumns="repeat(7, min-content)"
+        justifyContent="center"
+        justifyItems="center"
+      >
+        {weekdays.map((day) => (
+          <GridItem textTransform="uppercase" color="gray.400" mb="2">
+            {format(day, "EEE")}
+          </GridItem>
         ))}
-    </Grid>
+        {Object.entries(days)
+          .sort(([a], [b]) => Number.parseInt(a) - Number.parseInt(b))
+          .map(([day, trackers]) => (
+            <Day
+              key={day}
+              day={day}
+              trackers={trackers}
+              mx="2px"
+              my="8px"
+              onChange={onChange}
+            />
+          ))}
+      </Grid>
+    </>
   )
 }
