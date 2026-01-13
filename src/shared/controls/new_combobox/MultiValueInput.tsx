4f3ac90cf0594738ce2225f76c9980cabe9fdd3e
Erin Zimmer <ejzimmer@gmail.com>
2026-01-13 18:30:13 +1100

    basic multi select combobox styling

--- src/shared/controls/new_combobox/MultiValueInput.tsx
+++ src/shared/controls/new_combobox/MultiValueInput.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef, useState } from "react"
+import { XIcon } from "../../icons/X"
 import { SearchInputProps, SearchInput } from "./SearchInput"
 import { OptionType } from "./types"
 
@@ -17,23 +19,48 @@ export function MultiValueInput<T extends OptionType>({
   onChangeSearchTerm,
   ...searchInputProps
 }: MultiValueInputProps<T>) {
+  const valuesRef = useRef<HTMLUListElement>(null)
+  const [valuesWidth, setValuesWidth] = useState(0)
+
+  useEffect(() => {
+    if (!valuesRef.current) return
+
+    setValuesWidth(valuesRef.current.getBoundingClientRect().width)
+  }, [value])
+
+  const containerStyles = {
+    "--input-padding": valuesWidth + 4 + "px",
+  } as React.CSSProperties
+
   return (
-    <div className="multi-value-container">
+    <div className="multi-value-container" style={containerStyles}>
       <SearchInput
         value={searchInputValue}
         onChange={onChangeSearchTerm}
         {...searchInputProps}
       />
       <div>
-        <ul>
+        <ul ref={valuesRef} className="values">
           {value.map((v) => (
-            <li key={v.id}>
-              {v.label}{" "}
-              <button onClick={() => onRemoveValue(v)}>Remove {v.label}</button>
+            <li key={v.id} className="value">
+              {v.label}
+              <button
+                aria-label={`Remove ${v.label}`}
+                onClick={() => onRemoveValue(v)}
+                className="remove"
+              >
+                <XIcon width="14px" />
+              </button>
             </li>
           ))}
         </ul>
-        <button onClick={onRemoveAll}>Remove all</button>
+        <button
+          onClick={onRemoveAll}
+          className="remove-all"
+          aria-label="Remove all"
+        >
+          <XIcon width="16px" />
+        </button>
       </div>
     </div>
   )
